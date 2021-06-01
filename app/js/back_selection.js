b22a4c46b944bccd84b78e2f1898de7ea94b0c79
Niranjan Kumar <niranjanpid23@gmail.com>
2021-06-01 22:02:36 +0530

    added some functionality to back selection

--- app/js/back_selection.js
+++ app/js/back_selection.js
@@ -3,25 +3,33 @@ const back_select = document.querySelector('.back__selection');
 const close_back = document.querySelector('.back__selection .top .img');
 const header = document.querySelector('header');
 const sec_1 = document.querySelector('.section__1');
+const overlay_back = document.querySelector('.overlay2');
+const last_back_option = document.querySelector('.section__3 .options:last-child');
 
 function reveal(){
+    // back_select.classList.add('animate_atags_f');
+    // overlay_back.classList.add('animate_overlay_f');
+    header.style.zIndex = '0';
     back_select.style.visibility = 'visible';
     sec_1.style.border = 'none';
-    overlay.style.background = 'rgba(0, 0, 0, 0.658)';
-    overlay.style.height = `${body.scrollHeight}`;
-    console.log(overlay.style);
-    overlay.style.visibility = 'visible';
-    header.style.zIndex = '0';
-    console.log(overlay);
+    overlay_back.style.visibility = 'visible';
+    last_back_option.style.opacity = '1';
 }
 
 function close(){
     sec_1.style.border = '1px solid rgb(243, 243, 243)';
-    overlay.style.background = 'linear-gradient(rgba(0, 0, 0, 0.658),transparent)';
     back_select.style.visibility = 'hidden';
-    overlay.style.visibility = 'hidden';
+    overlay_back.style.visibility = 'hidden';
     header.style.zIndex = '1';
-    console.log(this);
+    last_back_option.style.opacity = '.6';
+    radio_options.forEach((option) =>{
+        option.children[0].children[1].removeAttribute('style');
+        option.removeAttribute('style');
+        try {
+          const hidden = option.children[3];
+          hidden.removeAttribute("style");
+        } catch (error) {}
+    })
 }
 
 button.addEventListener('click',reveal);
---
+++ app/js/radio_check.js
@@ -0,0 +1,42 @@
+const radio_options = document.querySelectorAll(".hover");
+const hidden_btn = document.querySelectorAll(".hidden .green");
+
+function display_option(e) {
+  const label = this.children[0].children[1];
+  console.log(e);
+  if (e.target.className === "green" || e.target.parentElement.className === 'hidden' || e.target.localName == 'input') {
+    return;
+  } else {
+    radio_options.forEach((option) => {
+      const opt = option.children[0].children[1];
+      if (opt === label && opt.getAttribute("data-flag") === "false") {
+        try {
+          const hidden = option.children[3];
+          hidden.style.display = "block";
+        } catch (error) {}
+        this.style.border = '2px solid hsl(176, 50%, 47%)';
+        opt.style.background = "hsl(176, 50%, 47%)";
+        opt.style.boxShadow = "inset 0 0 0 4px white";
+        opt.style.border = "2px solid hsl(176, 50%, 47%)";
+        opt.setAttribute("data-flag", "true");
+    } else if (opt.getAttribute("data-flag") === "true") {
+        try {
+            const hidden = option.children[3];
+            hidden.removeAttribute("style");
+        } catch (error) {}
+        option.removeAttribute('style');
+        opt.removeAttribute("style");
+        opt.setAttribute("data-flag", "false");
+      }
+    });
+  }
+}
+
+function success(e) {
+  console.log(this);
+}
+
+radio_options.forEach((option) =>
+  option.addEventListener("click", display_option)
+);
+hidden_btn.forEach((btn) => btn.addEventListener("click", success));
